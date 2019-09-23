d36caee77cc2191dcfda4b05c80ec709c35b4803
Junho Park <jhpark870708@daum.net>
2019-09-23 13:18:22 +0900

    init commit

---
+++ src/main/java/kr/co/itcen/guestbook/controller/GuestbookServlet.java
@@ -0,0 +1,72 @@
+package kr.co.itcen.guestbook.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.co.itcen.guestbook.dao.GuestbookDao;
+import kr.co.itcen.guestbook.vo.GuestbookVo;
+
+public class GuestbookServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+
+		String action = request.getParameter("a");
+
+		if ("add".equals(action)) {
+
+			String name = request.getParameter("name");
+			String password = request.getParameter("password");
+			String contents = request.getParameter("message");
+
+			GuestbookVo vo = new GuestbookVo();
+			vo.setName(name);
+			vo.setPassword(password);
+			vo.setContents(contents);
+
+			new GuestbookDao().insert(vo);
+			response.sendRedirect(request.getContextPath() + "/guestbook");
+
+		} else if ("deleteform".equals(action)) {
+
+			// forwarding
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/deleteform.jsp");
+			rd.forward(request, response);
+
+		} else if ("delete".equals(action)) {
+
+			String no = request.getParameter("no");
+			String password = request.getParameter("password");
+
+			GuestbookVo vo = new GuestbookVo();
+			vo.setNo(Long.parseLong(no));
+			vo.setPassword(password);
+
+			new GuestbookDao().delete(vo);
+
+			response.sendRedirect(request.getContextPath());
+
+		} else {
+			List<GuestbookVo> list = new GuestbookDao().getList();
+			request.setAttribute("list", list);
+
+			// forwarding
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+			rd.forward(request, response);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
